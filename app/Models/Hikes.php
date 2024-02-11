da51dc9a8b9be23281021f5c92c646406a644681
rosmis <rosmis123@gmail.com>
2024-02-11 15:47:26 +0100

    init laravel api

---
+++ app/Models/Hikes.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Attributes
+ * 
+ * @property string title
+ * @property string excerpt
+ * @property string description
+ * @property string activity_type
+ * @property string difficulty
+ * @property int distance
+ * @property string duration
+ * @property int positive_elevation
+ * @property int negative_elevation
+ * @property string municipality
+ * @property int highest_point
+ * @property int lowest_point
+ * @property string location
+ * @property string ign_reference
+ * @property string ign_url
+ * @property bool is_return_starting_point
+ * @property Carbon created_at
+ * @property Carbon updated_at
+ * 
+ */
+
+class Hikes extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2024_02_11_135348_create_hikes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hikes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('excerpt');
+            $table->text('description');
+            $table->string('activity_type');
+            $table->string('difficulty');
+            $table->unsignedInteger('distance');
+            $table->string('duration');
+            $table->unsignedInteger('positive_elevation');
+            $table->unsignedInteger('negative_elevation');
+            $table->string('municipality');
+            $table->unsignedInteger('highest_point');
+            $table->unsignedInteger('lowest_point');
+            $table->string('location');
+            $table->string('ign_reference')->nullable();
+            $table->string('ign_url')->nullable();
+            $table->boolean('is_return_starting_point')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hikes');
+    }
+};
